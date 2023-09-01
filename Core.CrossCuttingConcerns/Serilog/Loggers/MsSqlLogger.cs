d403e75cdc7ed5d1ba16a3c4775c2770cc81b2fe
Sahin YAKICI <shnykc@hotmail.com>
2023-09-02 00:26:16 +0300

    feat: Twenty Third section was completed
    MsSql log implementation was completed.

---
+++ Core.CrossCuttingConcerns/Serilog/Loggers/MsSqlLogger.cs
@@ -0,0 +1,27 @@
+﻿using Core.CrossCuttingConcerns.Serilog.ConfigurationModels;
+using Core.CrossCuttingConcerns.Serilog.Messages;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Core;
+using Serilog.Sinks.MSSqlServer;
+
+namespace Core.CrossCuttingConcerns.Serilog.Loggers;
+
+public class MsSqlLogger:LoggerServiceBase
+{
+    public MsSqlLogger(IConfiguration configuration)
+    {
+        MsSqlConfiguration msSqlLogConfiguration =
+            configuration.GetSection("SerilogLogConfigurations:MsSqlConfiguration").Get<MsSqlConfiguration>() ??
+            throw new Exception(SerilogMessages.NullOptionsMessage);
+        MSSqlServerSinkOptions msSqlServerSinkOptions = new()
+        {
+            TableName = msSqlLogConfiguration.TableName,
+            AutoCreateSqlDatabase = msSqlLogConfiguration.AutoCreateSqlTable,
+        };
+        ColumnOptions columnOptions = new();
+
+        Logger seriLogConfig = new LoggerConfiguration().WriteTo.MSSqlServer(msSqlLogConfiguration.ConnectionString,msSqlServerSinkOptions,columnOptions:columnOptions).CreateLogger();
+        Logger = seriLogConfig;
+    }
+}
