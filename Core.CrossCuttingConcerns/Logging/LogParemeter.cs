57b7910b4458c8778d575fe0537ab69489b7f681
Sahin YAKICI <shnykc@hotmail.com>
2023-09-01 19:28:47 +0300

    feat: Twentieth section was ended
    Log implementation was implemented.

---
+++ Core.CrossCuttingConcerns/Logging/LogParemeter.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.AccessControl;
+
+namespace Core.CrossCuttingConcerns.Logging;
+
+public class LogParemeter
+{
+    public string Name { get; set; }
+    public object Value { get; set; }
+    public string Type { get; set; }
+
+    public LogParemeter()
+    {
+        Name = string.Empty;
+        Value=string.Empty;
+        Type = string.Empty;
+    }
+
+    public LogParemeter(string name, object value, string type)
+    {
+        Name = name;
+        Value = value;
+        Type = type;
+    }
+}
