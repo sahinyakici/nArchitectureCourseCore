864903bfbfec56636b3ae4d548a71478b141fa90
Sahin YAKICI <shnykc@hotmail.com>
2023-09-01 20:26:57 +0300

    feat: Twenty first section was ended
    Logging pipeline was added.

--- Core.CrossCuttingConcerns/Logging/LogParameter.cs
+++ Core.CrossCuttingConcerns/Logging/LogParameter.cs
@@ -2,20 +2,20 @@
 
 namespace Core.CrossCuttingConcerns.Logging;
 
-public class LogParemeter
+public class LogParameter
 {
     public string Name { get; set; }
     public object Value { get; set; }
     public string Type { get; set; }
 
-    public LogParemeter()
+    public LogParameter()
     {
         Name = string.Empty;
         Value=string.Empty;
         Type = string.Empty;
     }
 
-    public LogParemeter(string name, object value, string type)
+    public LogParameter(string name, object value, string type)
     {
         Name = name;
         Value = value;
