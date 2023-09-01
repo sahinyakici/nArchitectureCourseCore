e692617afe7c4e69afdc67eb5e1de561db7452a3
Sahin YAKICI <shnykc@hotmail.com>
2023-09-01 13:19:21 +0300

    feat: Sixteenth section was ended.
    Add caching implementation with MediatR pipelines was implemented.

---
+++ Core.Application/Pipelines/Caching/CachingBehavior.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using System.Text.Json;
+using MediatR;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace Core.Application.Pipelines.Caching;
+
+public class CachingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>, ICachableRequest
+{
+    private readonly CacheSettings _cacheSettings;
+    private readonly IDistributedCache _cache;
+
+    public CachingBehavior(CacheSettings cacheSettings, IDistributedCache cache)
+    {
+        _cacheSettings = cacheSettings;
+        _cache = cache;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        if (request.BypassCache)
+        {
+            return await next();
+        }
+
+        TResponse response;
+        byte[]? cacheResponse = await _cache.GetAsync(request.CacheKey, cancellationToken);
+        if (cacheResponse != null)
+        {
+            response = JsonSerializer.Deserialize<TResponse>(Encoding.Default.GetString(cacheResponse));
+        }
+        else
+        {
+            response = await getResponseAndAddToCache(request,next,cancellationToken);
+        }
+
+        return response;
+    }
+
+    private async Task<TResponse> getResponseAndAddToCache(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        TResponse response = await next();
+        TimeSpan slidingExpration = request.SlidingExpiration ?? TimeSpan.FromDays(_cacheSettings.SlidingExpiration);
+        DistributedCacheEntryOptions cacheEntryOptions = new() { SlidingExpiration = slidingExpration};
+        byte[] serializedData = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(response));
+        await _cache.SetAsync(request.CacheKey, serializedData, cacheEntryOptions, cancellationToken);
+        return response;
+    }
+}
